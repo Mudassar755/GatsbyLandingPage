68924baca266c813d2cf300e8ef088e2bb06e2bd
mudassar755 <mudasar.se@gmail.com>
2021-11-04 11:44:36 +0500

    code updated according to design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,51 +1,90 @@
-import React from 'react'
-import seeMore from '../images/see-more-white.svg'
+import React from "react";
+import contact from "../images/contact-light.svg";
 
 const Footer = () => {
-    return (
-        <footer class="footer_area">
-                <div class="container">
-                    <div class="row px-3">
-                        <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
-                            <div class="footer_social text-left">
-                                <p>GUILLERMO GASCON</p>
-                                <input type="email" name="email" placeholder="Enter Email" className="mw-100 " />
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
-                        <div class="footer_social text-left">
-                                <p>GUILLERMO GASCON</p>
-                                <ul className="pl-4">
-                                    <li><a href="#">Link One</a></li>
-                                    <li><a href="#">Link Two</a></li>
-                                    <li><a href="#">Link Three</a></li>
-                                    <li><a href="#">Link Fourn</a></li>
-                                    <li><a href="#">Link Five</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
-                        <div class="footer_social text-left">
-                                <p>GUILLERMO GASCON</p>
-                                <ul className="pl-4">
-                                    <li><a href="#"><i class="fab fa-facebook-f"></i> Link One</a></li>
-                                    <li><a href="#"><i class="fab fa-twitter"></i> Link Two</a></li>
-                                    <li><a href="#"><i class="fab fa-instagram"></i> Link Three</a></li>
-                                    <li><a href="#"><i class="fab fa-linkedin-in"></i> Link Four</a></li>
-                                    <li><a href="#"><i class="fab fa-yelp"></i> Link Five</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
-                            <div class="text-left">
-                                {/* <p>We Accept</p> */}
-                                <img src={seeMore} alt="Welcome Vactor" width="100" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-    )
-}
+  return (
+    <footer class="footer_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2 col-md-2 col-6" data-aos="fade-up">
+            <div class="footer_social text-left">
+              <p>GUILLERMO GASCON</p>
+              <form className="position-relative">
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Email"
+                className="mw-100 "
+              />
+              <button className="position-absolute submit-btn"><i class="fas fa-long-arrow-alt-right"></i></button>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
+            <div class="footer_social text-left pl-3">
+              <p>Tema Legal</p>
+              <ul>
+                <li>
+                  <a href="#">Politica privacidad</a>
+                </li>
+                <li>
+                  <a href="#">Cookies</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-6" data-aos="fade-up">
+            <div class="footer_social text-left pl-3">
+              <p>Categorias</p>
+              <ul>
+                <li>
+                  <a href="#">Podcast de SEO</a>
+                </li>
+                <li>
+                  <a href="#">Newsletter</a>
+                </li>
+                <li>
+                  <a href="#">Canal YouTube</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-2 col-6" data-aos="fade-up">
+            <div class="footer_social text-left">
+              <p>Social Media</p>
+              <ul>
+                <li>
+                  <a href="#">
+                    <i class="fab fa-linkedin-in"></i> Linkedin
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fab fa-twitter"></i> Twitter
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fab fa-instagram"></i> Instagram
+                  </a>
+                </li>
+                {/* <li><a href="#"><i class="fab fa-facebook-f"></i> Linkedin</a></li>
+                                    <li><a href="#"><i class="fab fa-yelp"></i> Link Five</a></li> */}
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-2 col-6" data-aos="fade-up">
+            <div class="text-left">
+              {/* <p>We Accept</p> */}
+              <a href="#" className="contact-btn">
+                <img src={contact} alt="Welcome Vactor" width="100" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 import heroImg from "../images/hero-img.jpg";
-import seeMoreWhite from "../images/see-more-white.svg";
-import seeMoreBlack from "../images/see-more-black.svg";
-import work1 from "../images/work1.jpg";
-import work3 from "../images/work3.jpg";
-import work4 from "../images/work5.jpg";
+import contactLight from "../images/contact-light.svg";
+import contactDark from "../images/contact-dark.svg";
+
+import heroImg1 from "../images/hero-img1.png";
+import heroImg2 from "../images/hero-img2.png";
+import heroImg3 from "../images/hero-img3.png";
+import heroImg4 from "../images/hero-img4.png";
+
+import work1 from "../images/work1.png";
+import work2 from "../images/work2.png";
+import work3 from "../images/work3.png";
+import work4 from "../images/work4.png";
+
+import project1 from "../images/proj1.png";
+import project2 from "../images/proj2.png";
+import project3 from "../images/proj3.png";
+
 import Layout from "../components/Layout";
 import AOS from "aos";
 
@@ -17,39 +29,38 @@ const IndexPage = () => {
     });
     AOS.refresh();
 
-
     // const degreeToRadian = (angle) => {
     //   return angle * (Math.PI / 180);
     // };
-    
+
     // const radius = 75;
     // const diameter = radius * 2;
-    
+
     // const circle = document.querySelector("#circular-text");
     // circle.style.width = `${diameter}px`;
     // circle.style.height = `${diameter}px`;
-    
+
     // const text = circle.innerText;
     // const characters = text.split("");
     // circle.innerText = null;
-    
+
     // const startAngle = -90;
     // const endAngle = 270;
     // const angleRange = endAngle - startAngle;
-    
+
     // const deltaAngle = angleRange / characters.length;
     // let currentAngle = startAngle;
-    
+
     // characters.forEach((char, index) => {
     //   const charElement = document.createElement("span");
     //   charElement.innerText = char;
     //   const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
     //   const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
-    
+
     //   const transform = `translate(${xPos}px, ${yPos}px)`;
     //   const rotate = `rotate(${index * deltaAngle}deg)`;
     //   charElement.style.transform = `${transform} ${rotate}`;
-    
+
     //   currentAngle += deltaAngle;
     //   circle.appendChild(charElement);
     // });
@@ -64,15 +75,17 @@ const IndexPage = () => {
           <section className="hero_content_area">
             <div className="container pt-5">
               <div className="row mb-3" data-aos="fade-up">
-                <div className="col-lg-6" >
+                <div className="col-lg-6">
                   <div className="hero_title">
                     <h1>Guillermo Gascon</h1>
                   </div>
                 </div>
                 <div className="col-lg-6 d-flex align-items-center justify-content-center">
                   <div className="see-more">
-                  {/* <div id='circular-text'>contact</div> */}
-                    <img src={seeMoreWhite} alt="Hero Vactor" />
+                    {/* <div id='circular-text'>contact</div> */}
+                    <a href="#" className="contact-btn">
+                    <img src={contactLight} alt="Hero Vactor" />
+                    </a>
                   </div>
                 </div>
               </div>
@@ -80,14 +93,13 @@ const IndexPage = () => {
                 <div className="col-lg-6">
                   <div className="hero_content">
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Commodi laudantium placeat iste doloribus assumenda hic
-                      mollitia reprehenderit quos nihil, officiis ducimus
-                      aspernatur blanditiis consequuntur dolore dignissimos
+                      ¡Hola! Soy Guillermo Gascón, SEO afincado en Zaragoza con
+                      aspiraciones de vivir frente al mar. Trabajo en desarrollo
+                      web y marketing junto a mis dos socios en TheCookies.
                     </p>
                   </div>
-                  </div>
-                  <div className="col-lg-6">
+                </div>
+                <div className="col-lg-6">
                   <div className="social-links text-white">
                     <ul className="text-left">
                       <li className="mt-2">
@@ -105,15 +117,27 @@ const IndexPage = () => {
               </div>
 
               <div className="row my-3" data-aos="fade-up">
-                <div className="col-lg-6">
+                <div className="col-lg-7">
                   <div className="hero_image">
-                    <img src={heroImg} alt="" />
+                    <div className="hero-img1">
+                      <img src={heroImg1} />
+                      </div>
+                    <div className="hero-img2">
+                      <img src={heroImg2} />
+                      </div>
+                    <div className="hero-img3">
+                      <img src={heroImg3} />
+                      </div>
+                    <div className="hero-img4">
+                      <img src={heroImg4} />
+                      </div>
+                    {/* <img src={heroImg} alt="" /> */}
                   </div>
                 </div>
-                <div className="col-lg-6 text-center">
+                <div className="col-lg-5 text-center py-lg-5">
                   {/* <div className="logo-icons">
                     <div className="see-more" data-aos="fade-up">
-                      <img src={seeMoreWhite} alt="Hero Vactor" />
+                      <img src={contactLight} alt="Hero Vactor" />
                     </div>
                     <div className="social-links text-white mt-lg-5 pt-lg-5">
                       <ul className="text-left">
@@ -131,16 +155,14 @@ const IndexPage = () => {
                   </div> */}
                   <div className="hero_content mt-5 text-left px-lg-5">
                     <p className="mt-lg-3">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Quo, placeat temporibus obcaecati adipisci sit dolorem
-                      asperiores reprehenderit tenetur fugit eius, nemo nesciunt
-                      officia exercitationem ab ad est aspernatur eum iste.
+                      Estratega nato, no concibo que los proyectos en los que
+                      trabajo no tengan un horizonte definido. Me gusta trazar
+                      un plan y llevarlo a cabo
                     </p>
                     <p className="mt-lg-3">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Quo, placeat temporibus obcaecati adipisci sit dolorem
-                      asperiores reprehenderit tenetur fugit eius, nemo nesciunt
-                      officia exercitationem ab ad est aspernatur eum iste.
+                      Además, se me da bien estar en medio. Es una cualidad poco
+                      común y la aprovecho para gestionar proyectos y procesos
+                      permitiendo que todo el trabajo fluya según lo previsto.
                     </p>
                   </div>
                 </div>
@@ -157,15 +179,16 @@ const IndexPage = () => {
             <div className="container">
               <div className="row my-4">
                 <div className="col-lg-5" data-aos="fade-right">
-                  <div className="work_seaction_title" >
+                  <div className="work_seaction_title">
                     <h1>Trabajos</h1>
                   </div>
                 </div>
                 <div className="col-lg-7" data-aos="fade-left">
                   <div className="work_seaction_content">
                     <p>
-                      We identify, detect, protect and defend against
-                      Cybersecurity Attacks.
+                      Aquí tiienes algunas de las empresas donde he invertido mi
+                      tiempo y los proyectos personales más divertidos donde
+                      participo.
                     </p>
                   </div>
                 </div>
@@ -180,7 +203,7 @@ const IndexPage = () => {
                       </h3>
                     </div>
                     <div className="workTwo" data-aos="fade-right">
-                      <img src={work3} alt="" className="w-100" />
+                      <img src={work2} alt="" className="w-100" />
                       <h3>
                         <strong>Project Two</strong>
                       </h3>
@@ -195,7 +218,7 @@ const IndexPage = () => {
                         <strong>Project Three</strong>
                       </h3>
                     </div>
-                    <div className="workFour"data-aos="fade-left" >
+                    <div className="workFour" data-aos="fade-left">
                       <img src={work4} alt="" className="w-100" />
                       <h3>
                         <strong>Project Four</strong>
@@ -212,7 +235,7 @@ const IndexPage = () => {
               <div className="col-12 text-center">
                 <div className="seemore_image" data-aos="fade-right">
                   <img
-                    src={seeMoreBlack}
+                    src={contactDark}
                     alt="cyberscurity Vactor"
                     width="150"
                   />
@@ -239,19 +262,19 @@ const IndexPage = () => {
               <div className="row">
                 <div className="col-lg-8 my-3">
                   <div className="project1" data-aos="fade-up">
-                    <img src={work3} alt="" />
+                    <img src={project1} alt="" />
                     <h3>Proyectos</h3>
                   </div>
                 </div>
                 <div className="col-lg-6 my-3">
                   <div className="project2" data-aos="fade-up">
-                    <img src={work3} alt="" />
+                    <img src={project2} alt="" />
                     <h3>Proyectos</h3>
                   </div>
                 </div>
                 <div className="col-lg-6 my-3">
                   <div className="project3" data-aos="fade-up">
-                    <img src={work3} alt="" />
+                    <img src={project3} alt="" />
                     <h3>Proyectos</h3>
                   </div>
                 </div>
@@ -264,11 +287,13 @@ const IndexPage = () => {
               {/* Hero Image */}
               <div className="col-12 text-center">
                 <div className="seemore_image" data-aos="fade-right">
+                <a href="#" className="contact-btn">
                   <img
-                    src={seeMoreBlack}
+                    src={contactDark}
                     alt="cyberscurity Vactor"
                     width="150"
                   />
+                  </a>
                 </div>
               </div>
               {/* Hero Image */}
